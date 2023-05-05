8a823ee0442b62f163a7fd2b3eb8b20b5f86b309
Vladimir Below <laitvladimir@yandex.ru>
2023-05-05 09:58:46 +0300

    U: Flag
    Signed-off-by: Vladimir Below <laitvladimir@yandex.ru>

--- src/components/pages/app.tsx
+++ src/components/pages/app.tsx
@@ -14,16 +14,16 @@ const [ isShow, setIsShow ] = createSignal(true);
 				onclick={() => setIsShow(!isShow())}
 			>
 				<Show when={isShow()}>
-					<h1 class="font-Roboto text-10rem text-red-500">Крым НАШ</h1>
+					<h1 class="font-Roboto text-2rem lg:text-10rem text-red-500">Крым НАШ</h1>
 				</Show>
 				<Show when={!isShow()}>
-					<h1 class="font-Roboto text-10rem text-red-500">Панки хой</h1>
+					<h1 class="font-Roboto text-2rem lg:text-10rem text-red-500">Панки хой</h1>
 				</Show>
 			</div>
 			<div class="flex-col-center h-80vh">
-				<div class="w-50vw h-10vh bg-white"></div>
-				<div class="w-50vw h-10vh bg-blue-500"></div>
-				<div class="w-50vw h-10vh bg-red-500"></div>
+				<div class="w-90vw lg:w-50vw h-10vh bg-white"></div>
+				<div class="w-90vw lg:w-50vw h-10vh bg-blue-500"></div>
+				<div class="w-90vw lg:w-50vw h-10vh bg-red-500"></div>
 			</div>
 		</div>
 	);
