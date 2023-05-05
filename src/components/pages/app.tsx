6478f514bb2f18b6589006f8f27dc5c535053cfa
Vladimir Below <laitvladimir@yandex.ru>
2023-05-05 09:49:35 +0300

    N: Init
    Signed-off-by: Vladimir Below <laitvladimir@yandex.ru>

--- src/components/pages/app.tsx
+++ src/components/pages/app.tsx
@@ -1,14 +1,30 @@
 import { Title } from "@molecules/title";
 import { createRoute } from "atomic-router";
 
-import type { Component } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 
 export const appRoute = createRoute();
 
 export const App: Component = () => {
+const [ isShow, setIsShow ] = createSignal(true);
 	return (
-		<div class="w-screen h-screen flex-center">
-			<Title />
+		<div class="w-screen h-screen bg-dark-900 flex-col-center">
+			<div 
+				class="flex-center h-20vh"
+				onclick={() => setIsShow(!isShow())}
+			>
+				<Show when={isShow()}>
+					<h1 class="font-Roboto text-10rem text-red-500">Крым НАШ</h1>
+				</Show>
+				<Show when={!isShow()}>
+					<h1 class="font-Roboto text-10rem text-red-500">Панки хой</h1>
+				</Show>
+			</div>
+			<div class="flex-col-center h-80vh">
+				<div class="w-50vw h-10vh bg-white"></div>
+				<div class="w-50vw h-10vh bg-blue-500"></div>
+				<div class="w-50vw h-10vh bg-red-500"></div>
+			</div>
 		</div>
 	);
 };
